2d496b890ec90fd073b2c48d03876dcc1b5bb8f2
Jungram <yuns951213@gmail.com>
2024-03-19 16:09:23 +0900

    #1 스프링부트 환경설정 진행

---
+++ src/test/java/org/pratice/donemile/DonemileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pratice.donemile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonemileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
