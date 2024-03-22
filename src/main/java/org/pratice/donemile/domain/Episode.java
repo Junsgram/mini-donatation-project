5004937d2994dc951e62e8fe1935f7e5a8940e79
Jungram <yuns951213@gmail.com>
2024-03-22 16:45:12 +0900

    #6 - 테이블 별 Entity 클래스 구현 BaseEntity 생성일자 및 수정일자 추상클래스 구현 테스트 하위 폴더 생성

---
+++ src/main/java/org/pratice/donemile/domain/Episode.java
@@ -0,0 +1,46 @@
+package org.pratice.donemile.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.pratice.donemile.constant.BoardStatus;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+@ToString
+@Builder
+@Entity
+@SequenceGenerator(name="myEpisodeSeq", sequenceName = "epi_seq", initialValue = 1, allocationSize = 1)
+public class Episode extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "myEpisodeSeq")
+    @Column(name="episode_id")
+    private Long id;
+
+    @Column(length = 100, nullable = false) private String title;
+    @Column(length = 500, nullable = false) private String summary;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false) @ColumnDefault("0") private int goalAccount;
+    @Column(nullable = false) @ColumnDefault("0") private int currentAccount;
+    @Enumerated(EnumType.STRING) private BoardStatus boardStatus;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="donor_id")
+    private Donor donor;
+
+    @ToString.Exclude
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name="img_id")
+    private List<EpisodeImg> episodeImg;
+
+
+}
